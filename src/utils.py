c8ee7032546ee141a630e8a6a12ff5ace8279b37
Basil Foubert <basil.foubert@gmail.com>
2026-05-08 14:28:32 +0200

    feat: src/utils.py - save_jsonl

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import json
+from pathlib import Path
+
+JsonlData = list[dict]
 
 
-def load_jsonl(file_path: str) -> list[dict]:
+def load_jsonl(file_path: str | Path) -> JsonlData:
     data = []
     with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
@@ -10,3 +13,9 @@ def load_jsonl(file_path: str) -> list[dict]:
             except json.JSONDecodeError:
                 continue
     return data
+
+
+def save_jsonl(data: JsonlData, file_path: str | Path) -> None:
+    with open(file_path, "w", encoding="utf-8") as file:
+        for item in data:
+            file.write(json.dumps(item, ensure_ascii=False) + "\n")
